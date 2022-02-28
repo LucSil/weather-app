6f34a7369aece28e53be522c9ed45a7e12af7b56
LucSil <luciansilva351@gmail.com>
2022-02-28 11:46:31 +0100

    Initial commit of files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template></template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  box-sizing: border-box;
+  color: #2c3e50;
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+}
+</style>
